009c664c19dcd5899ccc63da1c7a7e1e830e553f
FerhanGit <fismailov@mailjet.com>
2016-08-25 16:47:43 +0300

    update

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,9 +1,9 @@
 <?php
 
 define("HOST", "localhost");
-define("DB", "gozbetr3_gozbite");
-define("WEBUSER", "gozbetr3_gozbite");
-define("WEBPASSWORD", "gozbite0505");
-define("ADMINPASSWORD", "gozbite0505");
+define("DB", "gozbite");
+define("WEBUSER", "root");
+define("WEBPASSWORD", "");
+define("ADMINPASSWORD", "");
    
 ?>
